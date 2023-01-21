27f871c93a659fb10933d592e541150e003a851b
Alyona <alyonabe@yandex.ru>
2023-01-21 23:05:22 +0300

    hw Spring Boot fix2

--- spring-03_hw2/src/main/java/ru/otus/spring/hw2/service/QuestionServiceImpl.java
+++ spring-03_hw2/src/main/java/ru/otus/spring/hw2/service/QuestionServiceImpl.java
@@ -1,9 +1,7 @@
 package ru.otus.spring.hw2.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
-import ru.otus.spring.hw2.config.ApplicationProps;
 import ru.otus.spring.hw2.dao.QuestionDao;
 
 import java.util.Scanner;
@@ -12,37 +10,30 @@ import java.util.Scanner;
 public class QuestionServiceImpl implements QuestionService {
 
     private final QuestionDao dao;
-    private final MessageSource messageSource;
-    private final ApplicationProps applicationProps;
+    private final LocaleMessage localeMessage;
+
 
     @Autowired
-    public QuestionServiceImpl(QuestionDao dao,MessageSource messageSource, ApplicationProps applicationProps) {
+    public QuestionServiceImpl(QuestionDao dao, LocaleMessage localeMessage) {
         this.dao = dao;
-        this.messageSource = messageSource;
-        this.applicationProps=applicationProps;
-
-
+        this.localeMessage = localeMessage;
     }
 
     public void getQuestions() {
         introduceTest();
         dao.findQuestions();
-        var messageLocalized = messageSource.getMessage("overTest.user", null, applicationProps.getLocale());
-        System.out.println(messageLocalized);
+        System.out.println(localeMessage.showMessage("overTest.user"));
     }
 
     public void introduceTest() {
-       Scanner in = new Scanner(System.in);
-       var messageLocalized = messageSource.getMessage("introduce.user", null, applicationProps.getLocale());
-       System.out.println(messageLocalized);
-       String str = in.nextLine();
-       var messageLocalized2 = messageSource.getMessage("inputNumber.user", new String[]{str},applicationProps.getLocale());
-       System.out.println(messageLocalized2);
+        Scanner in = new Scanner(System.in);
+        System.out.println(localeMessage.showMessage("introduce.user"));
+        String str = in.nextLine();
+        System.out.println(localeMessage.showMessage("inputNumber.user", str));
     }
 
     public void getResult() {
-        var messageLocalized = messageSource.getMessage("scoreTest.user", new Integer[]{dao.resultTest()},applicationProps.getLocale());
-        System.out.println(messageLocalized);
+        System.out.println(localeMessage.showMessage("scoreTest.user", dao.resultTest()));
     }
 
     @Override
