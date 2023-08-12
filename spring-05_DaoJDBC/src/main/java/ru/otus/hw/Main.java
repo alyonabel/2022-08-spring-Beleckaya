9601029a726231937c61f272a8282f2a04dc18b2
Alyona <alyonabe@yandex.ru>
2023-08-13 02:19:21 +0300

    hw Spring DaoJDBC final

--- spring-05_DaoJDBC/src/main/java/ru/otus/hw/Main.java
+++ spring-05_DaoJDBC/src/main/java/ru/otus/hw/Main.java
@@ -11,20 +11,6 @@ public class Main {
 
         ApplicationContext context = SpringApplication.run(Main.class);
 
-//        BookDao dao = context.getBean(BookDao.class);
-//
-//        System.out.println("All count: " + dao.count());
-//
-//        dao.insert("2",1,2);
-//
-//        System.out.println("All count: " + dao.count());
-//
-//        Book second = dao.getById(2);
-//
-//        System.out.println("Second id: " + second.getId() + "name: " + second.getName() + "author: " + second.getAuthor() + "genre: " + second.getGenre());
-//
-//        System.out.println(dao.getAll());
-
         Console.main(args);
     }
 }
